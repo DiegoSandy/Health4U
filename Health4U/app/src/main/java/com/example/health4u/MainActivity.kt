22b150e8a22b8b7a1191665ec1476d16d8225b09
Camila Cespedes <201904977@est.umss.edu>
2022-04-09 18:08:59 -0400

    Cams

---
+++ Health4U/app/src/main/java/com/example/health4u/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.health4u
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
+
+
